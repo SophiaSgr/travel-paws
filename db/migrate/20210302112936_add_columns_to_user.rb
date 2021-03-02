126e19b5f8b6bac9a4d9fcdb774d903ae7b51999
Sophia Sager <sophia.sager@outlook.de>
2021-03-02 12:30:15 +0100

    added columns

---
+++ db/migrate/20210302112936_add_columns_to_user.rb
@@ -0,0 +1,8 @@
+class AddColumnsToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :mobile_number, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+  end
+end
