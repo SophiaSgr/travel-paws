e3d2d00f581d9c84166f30d8e7dd7d986380ccb4
Altynai Adabaeva <altynai.a@posteo.de>
2021-03-02 15:24:35 +0100

    adding dropoff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_112936) do
+ActiveRecord::Schema.define(version: 2021_03_02_141112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_03_02_112936) do
     t.bigint "shelter_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "dropoff"
     t.index ["shelter_id"], name: "index_animals_on_shelter_id"
   end
 
