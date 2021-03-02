a7eb5d47fb631e75565d31fed129c364636b6367
Sophia Sager <sophia.sager@outlook.de>
2021-03-02 11:46:31 +0100

    added validations

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,5 @@
 class Request < ApplicationRecord
   belongs_to :animal
   belongs_to :user
+  validates :message, presence: true
 end
--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,4 +1,5 @@
 class Shelter < ApplicationRecord
   belongs_to :user
   has_many :animals
+  validates :name, :address, :description, presence: true
 end
