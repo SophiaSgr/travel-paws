78be600de21e6fe927ebc0053ec6413aa5a05d23
Sophia Sager <sophia.sager@outlook.de>
2021-03-04 15:54:36 +0100

    added requests model enum status and requests controller

--- db/migrate/20210304143949_create_requests.rb
+++ db/migrate/20210304143949_create_requests.rb
@@ -1,12 +1,12 @@
 class CreateRequests < ActiveRecord::Migration[6.0]
   def change
     create_table :requests do |t|
-      t.string :status
-      t.boolean :contract_signed
-      t.string :message
-      t.string :transportation_type
       t.references :animal, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
+      t.boolean :contract_signed
+      t.text :message
+      t.string :transportation_type
+      t.integer :status
 
       t.timestamps
     end
