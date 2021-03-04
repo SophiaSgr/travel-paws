f5e2ec74328309f4e991b4c4dd2661b83c9eccf9
Daniel Frieske <frieskedaniel@gmail.com>
2021-03-04 18:16:44 +0100

    added animals plus button

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :shelters, only: [ :index, :show, :new, :create ] do
-    resources :animals, only: [ :show, :create ] do
+    resources :animals, only: [ :show, :create, :new ] do
     end
   end
 end
