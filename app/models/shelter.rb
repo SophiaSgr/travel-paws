ccb0e9e600b8b4544881ac21d161e26df4b3dc14
Sophia Sager <sophia.sager@outlook.de>
2021-03-04 13:02:25 +0100

    added associatsions methods and controller

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,6 +1,6 @@
 class Shelter < ApplicationRecord
   has_many_attached :photos
-  belongs_to :user
   has_many :animals
+  belongs_to :user
   validates :name, :address, :description, presence: true
 end
