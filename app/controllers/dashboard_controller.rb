1b97d5d4f06810811f10046d23fed4c349e215d6
Sophia Sager <sophia.sager@outlook.de>
2021-03-04 12:04:25 +0100

    ok

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,13 +1,7 @@
 class DashboardController < ApplicationController
+def index
 
-  def index
-    if current_user.is_shelter_owner?
-  create instance variables for shelters, animals, incoming requests
-else
-  only outgoing requests
 end
-
-  end
 
 
 
