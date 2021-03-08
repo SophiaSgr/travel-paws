e68ecf8d01761c32945e747ff374c4db84ce9b86
Daniel Frieske <frieskedaniel@gmail.com>
2021-03-08 15:47:04 +0100

    just migrated

--- db/schema.rb
+++ db/schema.rb
@@ -74,7 +74,7 @@ ActiveRecord::Schema.define(version: 2021_03_08_094505) do
     t.boolean "contract_signed"
     t.text "message"
     t.string "transportation_type"
-    t.integer "status"
+    t.integer "status", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["animal_id"], name: "index_requests_on_animal_id"
