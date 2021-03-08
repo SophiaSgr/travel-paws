a95ac900b1ef007f02207f609b7291e7f8ba1bdd
Daniel Frieske <frieskedaniel@gmail.com>
2021-03-08 17:25:33 +0100

    added more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -79,16 +79,61 @@ andy = User.create!(
 
 puts 'CREATED ANIMAL FRIENDS'
 # -----------------------------------------------------------new seeds shelters
-protectimals = Shelter.new(
-  name: "Protectimal",
-  address: "Kithnos, Greece",
+hillvalleyshelter = Shelter.new(
+  name: "Hill Valley Shelter",
+  address: "Agrinio, Greece",
   description: "We are a shelter that takes in any kind of stray animals in order to give them another shot at a peaceful life. Currently we have over 96 animals in our care and appreciate any help the we can get. Especially in terms of funding and transportation as we are a small, non-governmental establishment.",
   user: savannah
 )
-file = URI.open('https://spcatampabay.org/wp-content/uploads/home_image_banner.jpg')
-protectimals.photo.attach(io: file, filename: 'avatar.jpg', content_type: 'image/jpg')
-protectimals.save
-protectimals.save
+file = URI.open('https://www.mercurynews.com/wp-content/uploads/2017/10/sjm-l-shelter-1101-22.jpg?w=620')
+hillvalleyshelter.photo.attach(io: file, filename: 'avatar.jpg', content_type: 'image/jpg')
+hillvalleyshelter.save
+hillvalleyshelter.save
+
+grevenashelter = Shelter.new(
+  name: "Grevena Shelter",
+  address: "Naxos, Greece",
+  description: "We are a shelter that takes in any kind of stray animals in order to give them another shot at a peaceful life. Currently we have over 96 animals in our care and appreciate any help the we can get. Especially in terms of funding and transportation as we are a small, non-governmental establishment.",
+  user: william
+)
+file = URI.open('https://www.aspcapro.org/sites/default/files/styles/image_component/public/resource/card/image/FIR_H7N2Cats_01_25_17_AKE_004.jpg?itok=2pY7VvgD')
+grevenashelter.photo.attach(io: file, filename: 'avatar.jpg', content_type: 'image/jpg')
+grevenashelter.save
+grevenashelter.save
+
+florinahome = Shelter.new(
+  name: "Florina Home",
+  address: "Florina, Greece",
+  description: "We are a shelter that takes in any kind of stray animals in order to give them another shot at a peaceful life. Currently we have over 96 animals in our care and appreciate any help the we can get. Especially in terms of funding and transportation as we are a small, non-governmental establishment.",
+  user: william
+)
+file = URI.open('https://www.stonnington.vic.gov.au/files/assets/public/services/stonnington-animal-pound-and-shelter.jpg?dimension=pageimagefullwidth&w=1140')
+florinahome.photo.attach(io: file, filename: 'avatar.jpg', content_type: 'image/jpg')
+florinahome.save
+florinahome.save
+
+dogsofveria = Shelter.new(
+  name: "Dogs of Veria",
+  address: "Veria, Greece",
+  description: "We are a shelter that takes in any kind of stray animals in order to give them another shot at a peaceful life. Currently we have over 96 animals in our care and appreciate any help the we can get. Especially in terms of funding and transportation as we are a small, non-governmental establishment.",
+  user: savannah
+)
+file = URI.open('https://images.foxtv.com/static.fox35orlando.com/www.fox35orlando.com/content/uploads/2020/11/764/432/GettyImages-1209292601.jpg?ve=1&tl=1')
+dogsofveria.photo.attach(io: file, filename: 'avatar.jpg', content_type: 'image/jpg')
+dogsofveria.save
+dogsofveria.save
+
+
+catcitykilkis = Shelter.new(
+  name: "Cat City Kilkis",
+  address: "Kilkis, Greece",
+  description: "We are a shelter that takes in any kind of stray animals in order to give them another shot at a peaceful life. Currently we have over 96 animals in our care and appreciate any help the we can get. Especially in terms of funding and transportation as we are a small, non-governmental establishment.",
+  user: savannah
+)
+file = URI.open('https://cloudfront-us-east-1.images.arcpublishing.com/tronc/73A4NANAGNCLPO36UVJDXCS7TY.jpg')
+catcitykilkis.photo.attach(io: file, filename: 'avatar.jpg', content_type: 'image/jpg')
+catcitykilkis.save
+catcitykilkis.save
 
 iloveanimals = Shelter.new(
   name: "I love Animals",
@@ -208,6 +253,238 @@ animalrescue.photo.attach(io: file, filename: 'avatar.jpg', content_type: 'image
 animalrescue.save
 
 p "CREATED #{Shelter.count} SHELTERS"
+
+
+# -------------------------------------------------new animals-------------------------------------------
+
+
+relaxo = Animal.new(
+  name: "Relaxo",
+  animal_type: "dog",
+  weight: "30kg",
+  size: "60cm",
+  age: "≈ 7",
+  medical_information: "neutered, flea treatment, vaccinated, chipped",
+  description: "Great, family friendly Dog. Brave and protective. Relaxo gets along well with other animals and people. He doesnt like to loud places, then he will go somewhere quiet. If there is a possibility to get her to Munich by plane, this is much preferred.",
+  race: "Labrador",
+  shelter: lifeforanimals,
+  dropoff: "Munich, Germany"
+)
+file = URI.open('https://ak.picdn.net/shutterstock/videos/15734035/thumb/1.jpg')
+relaxo.photos.attach(io: file, filename: 'relaxo.jpg', content_type: 'image/jpg')
+relaxo.save
+
+george = Animal.new(
+  name: "George",
+  animal_type: "cat",
+  weight: "8kg",
+  size: "23cm",
+  age: " ≈ 5 1/2",
+  medical_information: "neutered, flea treatment, vaccinated, chipped",
+  description: "George is just aweseom. He's super chilled, likes to be petted and enjoys strollling around. He has a great heart and has no pfoblem with children making him and family friendly companian.",
+  race: "unknown",
+  shelter: lifeforanimals,
+  dropoff: "Athens, Greece"
+)
+file = URI.open('https://www.gannett-cdn.com/-mm-/938db79d26e3ee729e6b7468e9f299d2e4ef16df/c=0-279-2491-1684/local/-/media/USATODAY/test/2013/09/05/1378400626002--NASBrd-08-11-2013-Tennessean-1-B004-2013-08-10-IMG-NAS-FATCAT-03jpg-1-1-P.jpg')
+george.photos.attach(io: file, filename: 'geroge.jpg', content_type: 'image/jpg')
+george.save
+
+hamlet = Animal.new(
+  name: "Hamlet",
+  animal_type: "dog",
+  weight: "2.3kg",
+  size: "27cm",
+  age: "≈ 1",
+  medical_information: "neutered, flea treatment, chipped, vaccination outstanding",
+  description: "Hamlet is a kind natured dog. He was left in front of our shelter, and was so scared. Throughout the time he became a very friedly little friend, everybody enjoys around. In that aspect he is very comfortable. He needs to be dropped of in Marseille",
+  race: "mixed",
+  shelter: lifeforanimals,
+  dropoff: "Marseille, France"
+)
+file = URI.open('https://d17fnq9dkz9hgj.cloudfront.net/uploads/2018/02/Affenpinscher-Cropped.jpg')
+hamlet.photos.attach(io: file, filename: 'hamlet.jpg', content_type: 'image/jpg')
+hamlet.save
+
+swiffer = Animal.new(
+  name: "Swiffer",
+  animal_type: "dog",
+  weight: "12.2kg",
+  size: "55cm",
+  age: "≈ 3",
+  medical_information: "neutered, flea treatment, chipped, vaccination outstanding",
+  description: "Swiffer is a lovely and beuatiful dog. Most of the time he is just relaxing on the floor and wiggeling around. Great dog if you dont have a mop to clean the floor. Needs to be dropped of in Berlin, Germany.",
+  race: "mixed",
+  shelter: lifeforanimals,
+  dropoff: "Berlin, Germany"
+)
+
+file = URI.open('https://i0.wp.com/bestlifeonline.com/wp-content/uploads/2019/01/coton-du-tulear.jpg?resize=1200%2C800&ssl=1')
+claude.photos.attach(io: file, filename: 'swiffer.jpg', content_type: 'image/jpg')
+claude.save
+
+shawn = Animal.new(
+  name: "Shawn",
+  animal_type: "cat",
+  weight: "3.6kg",
+  size: "25cm",
+  age: "≈ 2.5",
+  medical_information: "neutered, flea treatment, vaccinated, chipped, flight ready",
+  description: "Shawn is very kind natured. He enjoys the company of people and enjoys meeting strangers. Travelling by plane or car with him should be no problem.",
+  race: "red cat",
+  shelter: lifeforanimals,
+  dropoff: "Lissabon, Portugal"
+)
+file = URI.open('https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg')
+shawn.photos.attach(io: file, filename: 'shawn.jpg', content_type: 'image/jpg')
+shawn.save
+
+jeffy = Animal.new(
+  name: "Jeffy",
+  animal_type: "dog",
+  weight: "12kg",
+  size: "30cm",
+  age: "≈ 8",
+  medical_information: "neutered, flea treatment, vaccinated, chipped, heart medication, Congenital Disorder: troubles with coordination",
+  description: "Jeffy cannot see that well anymore and sometimes needs some guidance finding his spots in new environments. once he settles down he is very enjoyable and easy to go with. He loves snacks and loves to be patted. Transportation is fine for him. Drop-off in Hamburg, Berlin",
+  race: "unknown",
+  shelter: lifeforanimals,
+  dropoff: "Hamburg, Germany"
+)
+file = URI.open('https://images.pexels.com/photos/613862/pexels-photo-613862.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')
+jeffy.photos.attach(io: file, filename: 'jeffy.jpg', content_type: 'image/jpg')
+jeffy.save
+
+# smurf = Animal.new(
+#   name: "Smurf",
+#   animal_type: "dog",
+#   weight: "25.4kg",
+#   size: "56cm",
+#   age: "≈ 3",
+#   medical_information: "neutered, flea treatment, vaccinated, chipped, weak bladder",
+#   description: "Amadeus was dropped off in our shelter one year ago and since then has found a lovely family in Bucharest. He loves travelling by car with his head held out of the window. This makes him smile a lot. He would need a break every hours or so to go out to pee as he has a weak bladder. Travelling by plane should be no problem either, as he is fine being in a box. ",
+#   race: "Labrador",
+#   shelter: ourdogs,
+#   dropoff: "Berlin, Germany"
+# )
+# file = URI.open('https://images.unsplash.com/photo-1534283363159-b466d8abfcb3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxMTgwOTN8MHwxfHNlYXJjaHwxM3x8bGFicmFkb3J8ZW58MHx8fA&ixlib=rb-1.2.1&q=80&w=1080')
+# amadeus.photos.attach(io: file, filename: 'amadeus.jpg', content_type: 'image/jpg')
+# amadeus.save
+
+# nutmeg = Animal.new(
+#   name: "Nutmeg",
+#   animal_type: "cat",
+#   weight: "4kg",
+#   size: "27cm",
+#   age: "≈ 2",
+#   medical_information: "neutered, flea treatment, vaccinated, chipped, fractured leg, pain medication",
+#   description: "Nutmeg needs a way home to her forever family in Sevilla. She at this point has a cast around her right leg and is in the end stadium of healing from a fracture . Travelling by car would be preferred as we want her to be as relaxed as possible. Nutmeg has no problems riding in cars as she mostly sleeps and loves being pet while doing it.",
+#   race: "unknown",
+#   shelter: happytails,
+#   dropoff: "Sevilla, Spain"
+# )
+# file = URI.open('https://images.unsplash.com/photo-1605706275526-ded7b77e5e44?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxMTgwOTN8MHwxfHNlYXJjaHwzMHx8cmVzY3VlJTIwY2F0fGVufDB8fHw&ixlib=rb-1.2.1&q=80&w=1080')
+# nutmeg.photos.attach(io: file, filename: 'nutmeg.jpg', content_type: 'image/jpg')
+# nutmeg.save
+
+# igor = Animal.new(
+#   name: "Igor",
+#   animal_type: "dog",
+#   weight: "15kg",
+#   size: "48cm",
+#   age: "≈ 4",
+#   medical_information: "neutered, flea treatment, vaccinated, chipped",
+#   description: "Igor has found a family in Warsaw, Poland and needs means of transportation to get to his forever home. He walks well on a leash, is house trained, and knows basic commands. Travelling by car or plane is fine by him.",
+#   race: "Border Collie",
+#   shelter: animalrescue,
+#   dropoff: "Warsaw, Poland"
+# )
+# file = URI.open('https://images.unsplash.com/photo-1573507132549-e368aecf8f90?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxMTgwOTN8MHwxfHNlYXJjaHwxM3x8Ym9yZGVyJTIwY29sbGllfGVufDB8fHw&ixlib=rb-1.2.1&q=80&w=1080')
+# igor.photos.attach(io: file, filename: 'igor.jpg', content_type: 'image/jpg')
+# igor.save
+
+# milo = Animal.new(
+#   name: "Milo",
+#   animal_type: "dog",
+#   weight: "15kg",
+#   size: "44cm",
+#   age: "≈ 2",
+#   medical_information: "I'm Milo and have found a home in Groningen, the Netherlands. But I still don't know how to get there- can you help me? I am fine with travelling by car or plane but I do prefer a plane as I get car sick and dizzy. I love pets and treats and sleep a lot. I bark when I feel uncomfortable btu as long as your energy is good we will have no problems.",
+#   race: "unknown",
+#   shelter: furryfriends,
+#   dropoff: "Groningen, Netherlands"
+# )
+# file = URI.open('https://images.unsplash.com/photo-1593991393705-552675fe6f6d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxMTgwOTN8MHwxfHNlYXJjaHw0NHx8cmVzY3VlJTIwZG9nfGVufDB8fHw&ixlib=rb-1.2.1&q=80&w=1080')
+# milo.photos.attach(io: file, filename: 'milo.jpg', content_type: 'image/jpg')
+# milo.save
+
+# mrbean = Animal.new(
+#   name: "Mr. Bean",
+#   animal_type: "dog",
+#   weight: "4kg",
+#   size: "15cm",
+#   age: "≈ 4",
+#   medical_information: "neutered, flea treatment, vaccinated, chipped",
+#   description: "My name is Mr. Bean. I need to go to Ansbach in Germany and need support in getting there as walking is no option for me. Being in a box is not my favorite thing to do and I get scared sometimes. But don't worry. Just give me pets and tell me I'm okay and we will be fine. Will you be my travel guardian?",
+#   race: "mixed",
+#   shelter: ourdogs,
+#   dropoff: "Ansbach, Germany"
+# )
+# file = URI.open('https://images.unsplash.com/photo-1606501147522-c1830b14af02?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxMTgwOTN8MHwxfHNlYXJjaHwyfHxyZXNjdWUlMjBkb2d8ZW58MHx8fA&ixlib=rb-1.2.1&q=80&w=1080')
+# mrbean.photos.attach(io: file, filename: 'mrbean.jpg', content_type: 'image/jpg')
+# mrbean.save
+
+# bilbo = Animal.new(
+#   name: "Bilbo",
+#   animal_type: "dog",
+#   weight: "15kg",
+#   size: "48cm",
+#   age: "≈ 3",
+#   medical_information: "neutered, flea treatment, vaccinated, chipped",
+#   description: "Bilbo has found a family in Milan, Italy and needs means of transportation to get to his forever home. He walks well on a leash, is house trained, and knows basic commands. Travelling by car or plane is fine by him.",
+#   race: "unknown",
+#   shelter: laika,
+#   dropoff: "Milan, Italiy"
+# )
+# file = URI.open('https://images.unsplash.com/photo-1596838005467-a34b063fc0b5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxMTgwOTN8MHwxfHNlYXJjaHw0N3x8cmVzY3VlJTIwZG9nfGVufDB8fHw&ixlib=rb-1.2.1&q=80&w=1080')
+# bilbo.photos.attach(io: file, filename: 'bilbo.jpg', content_type: 'image/jpg')
+# bilbo.save
+
+# souna = Animal.new(
+#   name: "Souna",
+#   animal_type: "dog",
+#   weight: "12kg",
+#   size: "35cm",
+#   age: "≈ 4",
+#   medical_information: "neutered, flea treatment, vaccinated, chipped",
+#   description: "Souna is a gentle natured and kind dog. She needs to be taken to Turin, where her family will meet her at the aiport. Flying is preferred and the date is flexible- of course, the sooner the better!",
+#   race: "mixed",
+#   shelter: naplespaws,
+#   dropoff: "Turin, Italy"
+# )
+# file = URI.open('https://images.unsplash.com/photo-1584269939785-8892b6ece544?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxMTgwOTN8MHwxfHNlYXJjaHw1OHx8cmVzY3VlJTIwZG9nfGVufDB8fHw&ixlib=rb-1.2.1&q=80&w=1080')
+# souna.photos.attach(io: file, filename: 'souna.jpg', content_type: 'image/jpg')
+# souna.save
+
+# adonis = Animal.new(
+#   name: "Adonis",
+#   animal_type: "dog",
+#   weight: "28kg",
+#   size: "60cm",
+#   age: "≈ 5",
+#   medical_information: "neutered, flea treatment, vaccinated, chipped",
+#   description: "Adonis needs transportation from Crete to Athens. He is fine with going on a ferry and also plane is no problem. He is a calm dog who enjoys pets and cuddles and cant get enough of snacks! His famiyl will pick him up wherever he is delivered to in Athens.",
+#   race: "mixed",
+#   dropoff: "Athens, Greece",
+#   shelter: lifeforanimals
+# )
+# file = URI.open('https://images.unsplash.com/photo-1568137389085-3ccda743a644?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxMTgwOTN8MHwxfHNlYXJjaHw0Nnx8cmVzY3VlJTIwZG9nfGVufDB8fHw&ixlib=rb-1.2.1&q=80&w=1080')
+# adonis .photos.attach(io: file, filename: 'adonis.jpg', content_type: 'image/jpg')
+# adonis .save
+
+
+# -------------------------------------------------old animals -------------------------------------------
+
 
 fifi = Animal.new(
   name: "Fifi",
