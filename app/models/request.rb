d7978eb30ea50147638ea49118ccab21bb1b38f7
Daniel Frieske <frieskedaniel@gmail.com>
2021-03-08 14:59:36 +0100

    added some form for the passport

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,5 +2,6 @@ class Request < ApplicationRecord
   belongs_to :animal
   belongs_to :user
   validates :message, presence: true
+  has_one_attached :passport
   enum status: [ :pending, :accepted, :declined ]
 end
